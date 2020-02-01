e1d9d65058baaa38f8ad474f719f79bb2e304d65
cgobbo <corentin.gobbo@hotmail.fr>
2020-02-01 22:55:16 +0100

    tableau4

---
+++ Assets/Scripts/detectTouch.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class detectTouch : MonoBehaviour
+{
+    [SerializeField]
+    public SpriteRenderer background;
+
+    [SerializeField]
+    public Sprite blanc;
+
+    [SerializeField]
+    public Sprite noir;
+
+    public Boolean flag = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            Application.LoadLevel(Application.loadedLevel);
+        }
+        if (Input.GetKeyDown("p"))
+        {
+            if (flag)
+            {
+                background.sprite = noir;
+                flag = !flag;
+            }else{
+                background.sprite = blanc;
+                flag = !flag;
+            }
+           
+        }
+
+    }
+}
