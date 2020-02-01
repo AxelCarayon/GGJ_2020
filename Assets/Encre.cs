663a754bdde1c2fdca2fcb190e6c6c6b7883d875
jaffret laurine <laurinejaffret@orange.fr>
2020-02-01 20:05:29 +0100

    lv2 et debut menu

---
+++ Assets/Encre.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Encre : MonoBehaviour
+{
+    private Vector2 mousePos2D;
+    private Vector3 mousePos;
+
+    
+
+    void DestroyComponent()
+    {
+        // Supprime le corps rigide de l'objet de jeu
+        Destroy(GetComponent<Rigidbody>());
+    }
+
+    
+    void OnMouseDown()
+    {   
+        if (gomme.possession == true)
+        Destroy(gameObject);
+
+    }
+
+
+    
+    void Update()
+    {
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        
+    }
+}
---
+++ Assets/gomme.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gomme : MonoBehaviour
+{
+    
+    public static bool possession = false;
+    
+    
+    
+    private Vector2 mousePos2D;
+    private Vector3 mousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print(possession);
+
+    }
+    
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        print("oui");
+        if (other.gameObject.name == "gomme")
+        {
+            Destroy(other.gameObject);
+            possession = true;
+        }
+
+        
+
+    }
+}
