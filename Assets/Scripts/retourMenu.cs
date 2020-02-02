3113f825fdc818b216214891667d99bc95875f8f
AxelCarayon <axel.carayon@etud.univ-jfc.fr>
2020-02-02 15:45:37 +0100

    Ajout bouton menu

---
+++ Assets/Scripts/retourMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class retourMenu : MonoBehaviour
+{
+    private Vector2 mousePos2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene("menu2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
